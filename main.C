1298ccaea5f92f99f4d022b1712c8d39f897ae57
Gioele Barabucci <dev@gioelebarabucci.com>
2002-05-18 11:56:02 +0000

    Let's avoid old and deprecated headers
    svn path=/trunk/kdegames/ksokoban/; revision=156131

--- main.C
+++ main.C
@@ -17,7 +17,7 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-// #include <kuniqueapp.h>
+// #include <kuniqueapplication.h>
 #include <kapplication.h>
 #include <kimageio.h>
 #include <klocale.h>
